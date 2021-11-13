57d3b51ddaca953b82d944726b977b6f648135f3
Moreshwar Dalvi <moreshwar.dalvi@acclivistechnologies.com>
2021-11-13 18:12:49 +0530

    add react fast refresh for development

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,27 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 let mode = "development";
 let target = "web";
+const plugins = [
+  new CleanWebpackPlugin(),
+  new MiniCssExtractPlugin(),
+  new HtmlWebpackPlugin({ template: "./src/index.html" }),
+];
 
 if (process.env.NODE_ENV === "production") {
   mode = "production";
   target = "browserslist";
+} else {
+  plugins.push(new ReactRefreshWebpackPlugin());
 }
 module.exports = {
   mode: mode,
   target: target,
+
+  entry: "./src/index.js",
 
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -53,11 +63,7 @@ module.exports = {
     ],
   },
   resolve: { extensions: [".js", ".jsx"] },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),
-    new HtmlWebpackPlugin({ template: "./src/index.html" }),
-  ],
+  plugins: plugins,
   devtool: "source-map",
   devServer: {
     static: "./dist",
