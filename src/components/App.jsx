86cde51ad5188a3b6ec514ef8f68879989333e8d
Moreshwar Dalvi <moreshwar.dalvi@acclivistechnologies.com>
2021-11-13 16:44:06 +0530

    add image support to webpack

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
 import "../styles/index.scss";
 import Recipes from "./Recipes";
 
+import sword from "./../images/swc-sword.png";
+import swordSvg from "./../images/sword.svg";
+
 const App = () => {
   return (
     <>
@@ -8,9 +11,11 @@ const App = () => {
       <main>
         <section>
           <h1>Hey React dude!</h1>
+          <img src={sword} alt="sword" width="250" />
+          <img src={swordSvg} alt="swordsvg" width="250" />
         </section>
+        <Recipes />
       </main>
-      <Recipes />
     </>
   );
 };
